79b40e660f6fb43f156ada018fbf199151ad724d
Parker Ryan <aparkerryan27@gmail.com>
2022-01-25 16:50:11 -0600

    adding PWM functionality and pitch PID control

--- week1_student.cpp
+++ week1_student.cpp
@@ -29,6 +29,17 @@
 #define MAX_ROLL_ANGLE 45
 #define MAX_PITCH_ANGLE 45
 
+//motor constants 
+#define PWM_MAX 1300
+#define PWM_NEUTRAL 1100
+#define frequency 25000000.0
+#define LED0 0x6			
+#define LED0_ON_L 0x6		
+#define LED0_ON_H 0x7		
+#define LED0_OFF_L 0x8		
+#define LED0_OFF_H 0x9		
+#define LED_MULTIPLYER 4
+
 enum Ascale {
     AFS_2G = 0,
     AFS_4G,
@@ -64,6 +75,10 @@ float yaw=0;
 float pitch_angle=0;
 float roll_angle=0;
 
+//temp global variables for printing
+float roll_angle_accel = 0;
+float pitch_angle_accel = 0;
+
 // new globals for gyro
 float pitch_angle_gyro = 0;
 float roll_angle_gyro = 0;
@@ -78,6 +93,14 @@ Keyboard* shared_memory;
 int run_program=1;
 long prev_heartbeat_time = 0;
 int prev_heartbeat;
+
+
+
+//motor variables 
+int pwm;
+
+
+/* ------ IMU DATA -------- */
 
 void calibrate_imu()
 {
@@ -101,7 +124,7 @@ void calibrate_imu()
     pitch_calibration = pitch_temp;
 
     //Pretty Print Calibration Values
-    printf("Calibration Values (x_gyro, y_gyro, z_gyro, pitch, roll),  (%10.5f, %10.5f, %10.5f, %10.5f, %10.5f)\n\r",x_gyro_calibration,y_gyro_calibration,z_gyro_calibration,pitch_calibration,roll_calibration);
+    printf("Calibration Complete: (x_gyro, y_gyro, z_gyro, pitch, roll),  (%10.5f, %10.5f, %10.5f, %10.5f, %10.5f)\n\r",x_gyro_calibration,y_gyro_calibration,z_gyro_calibration,pitch_calibration,roll_calibration);
 }
 
 void read_imu()
@@ -209,8 +232,8 @@ void update_filter()
     time_prev=time_curr;
       
     //Roll and pitch from accel data - roll (y-axis), pitch (x-axis)
-    float pitch_angle_accel = (-atan2( imu_data[4], -imu_data[5]) / 0.017453) - pitch_calibration; // angle of rotation x
-    float roll_angle_accel =  (atan2( imu_data[3], -imu_data[5]) / 0.017453) - roll_calibration; // angle of rot y
+    pitch_angle_accel = (-atan2( imu_data[4], -imu_data[5]) / 0.017453) - pitch_calibration; // angle of rotation x
+    roll_angle_accel =  (atan2( imu_data[3], -imu_data[5]) / 0.017453) - roll_calibration; // angle of rot y
     
     //Change in Roll and Pitch from integrated gyro
     float roll_gyro_delta = imu_data[1] * imu_diff;
@@ -270,6 +293,124 @@ int setup_imu()
 }
 
 
+
+/* ------ MOTOR INTERACTION -------- */
+
+void init_pwm()
+{
+
+    pwm=wiringPiI2CSetup (0x40);
+    if(pwm==-1)
+    {
+      printf("-----cant connect to I2C device %d --------\n",pwm);
+     
+    }
+    else
+    {
+  
+      float freq =400.0*.95;
+      float prescaleval = 25000000;
+      prescaleval /= 4096;
+      prescaleval /= freq;
+      prescaleval -= 1;
+      uint8_t prescale = floor(prescaleval+0.5);
+      int settings = wiringPiI2CReadReg8(pwm, 0x00) & 0x7F;
+      int sleep	= settings | 0x10;
+      int wake 	= settings & 0xef;
+      int restart = wake | 0x80;
+      wiringPiI2CWriteReg8(pwm, 0x00, sleep);
+      wiringPiI2CWriteReg8(pwm, 0xfe, prescale);
+      wiringPiI2CWriteReg8(pwm, 0x00, wake);
+      delay(10);
+      wiringPiI2CWriteReg8(pwm, 0x00, restart|0x20);
+    }
+}
+
+
+
+void init_motor(uint8_t channel)
+{
+	int on_value=0;
+
+	int time_on_us=900;
+	uint16_t off_value=round((time_on_us*4096.f)/(1000000.f/400.0));
+
+	wiringPiI2CWriteReg8(pwm, LED0_ON_L + LED_MULTIPLYER * channel, on_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_ON_H + LED_MULTIPLYER * channel, on_value >> 8);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_L + LED_MULTIPLYER * channel, off_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_H + LED_MULTIPLYER * channel, off_value >> 8);
+	delay(100);
+
+	 time_on_us=1200;
+	 off_value=round((time_on_us*4096.f)/(1000000.f/400.0));
+
+	wiringPiI2CWriteReg8(pwm, LED0_ON_L + LED_MULTIPLYER * channel, on_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_ON_H + LED_MULTIPLYER * channel, on_value >> 8);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_L + LED_MULTIPLYER * channel, off_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_H + LED_MULTIPLYER * channel, off_value >> 8);
+	delay(100);
+
+	 time_on_us=1000;
+	 off_value=round((time_on_us*4096.f)/(1000000.f/400.0));
+
+	wiringPiI2CWriteReg8(pwm, LED0_ON_L + LED_MULTIPLYER * channel, on_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_ON_H + LED_MULTIPLYER * channel, on_value >> 8);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_L + LED_MULTIPLYER * channel, off_value & 0xFF);
+	wiringPiI2CWriteReg8(pwm, LED0_OFF_H + LED_MULTIPLYER * channel, off_value >> 8);
+	delay(100);
+
+}
+
+
+void set_PWM( uint8_t channel, float time_on_us)
+{
+  if(run_program==1)
+  {
+    if(time_on_us>PWM_MAX)
+    {
+      time_on_us=PWM_MAX;
+    }
+    else if(time_on_us<1000)
+    {
+      time_on_us=1000;
+    }
+  	uint16_t off_value=round((time_on_us*4096.f)/(1000000.f/400.0));
+  	wiringPiI2CWriteReg16(pwm, LED0_OFF_L + LED_MULTIPLYER * channel,off_value);
+  }
+  else
+  {  
+    time_on_us=1000;   
+  	uint16_t off_value=round((time_on_us*4096.f)/(1000000.f/400.0));
+  	wiringPiI2CWriteReg16(pwm, LED0_OFF_L + LED_MULTIPLYER * channel,off_value);
+  }
+}
+
+
+/* ----------- PID CONTROL ------------- */
+
+void pid_update(float pitch_reference = 0) 
+{
+    //args: pitch_reference is desired pitch
+    float P = 7;
+
+    float pitch_error = pitch_reference - pitch_angle;
+    float pwm = pitch_error * P;
+
+    if (pwm > (PWM_MAX - PWM_NEUTRAL)) pwm = (PWM_MAX - PWM_NEUTRAL);
+
+    set_PWM(0, PWM_NEUTRAL + pwm);
+    set_PWM(3, PWM_NEUTRAL + pwm);
+    set_PWM(1, PWM_NEUTRAL - pwm);
+    set_PWM(2, PWM_NEUTRAL - pwm);
+    
+    printf("%10.5f %10.5f %10.5f %10.5f\n", pitch_angle, pitch_angle_accel, (PWM_NEUTRAL + pwm), (PWM_NEUTRAL - pwm));
+
+}
+
+
+
+/* ------ SAFETY -------- */
+
 void setup_keyboard()
 {
 
@@ -297,8 +438,12 @@ void setup_keyboard()
 //when cntrl+c pressed, kill motors
 void trap(int signal)
 {
-   printf("ending program\n\r");
-   run_program=0;
+    printf("ending program\n\r");
+    run_program=0;
+    set_PWM(0,0);
+    set_PWM(1,0);
+    set_PWM(2,0);
+    set_PWM(3,0);
 }
 
 void safety_check() 
@@ -328,7 +473,8 @@ void safety_check()
         run_program=0;
     }
     
-    //If the heartbeat has not changed, check how long it's been since an update occured
+    /* 
+    //Keyboard Timeout
     if ( prev_heartbeat_time == 0  || prev_heartbeat != shared_memory->heartbeat ) {
         timespec_get(&te,TIME_UTC);
         prev_heartbeat_time=te.tv_nsec;
@@ -350,28 +496,50 @@ void safety_check()
             run_program=0;
         }
     } 
+    */
 
 }
+
+
+
+/* ------ MAIN -------- */
 
 int main (int argc, char *argv[])
 {
 
-    //Perform Setup
+    //Safety Setup
     setup_keyboard();
     signal(SIGINT, &trap);
+
+    //Motor Setup
+    init_pwm();
+    init_motor(0);
+    init_motor(1);
+    init_motor(2);
+    init_motor(3);
+    delay(1000);
+
+    //IMU Setup
     setup_imu();
     calibrate_imu();
-
-    //TODO: Print Calibrating.......... and also reset keyboard values
+    printf("Calibrating....");
+    
+    pid_update();
     while(run_program == 1)
     {
         //sleep(1);
         read_imu();
         update_filter();
         safety_check();
-        
+        pid_update();
         //printf("IMU Values: x_gyro= %10.5f, y_gyro = %10.5f, z_gyro = %10.5f, pitch =  %10.5f, roll = %10.5f\n\r",imu_data[0],imu_data[1],imu_data[2], pitch_angle, roll_angle);
         
     }
+    //turn off motors when program is ended
+    set_PWM(0,0);
+    set_PWM(1,0);
+    set_PWM(2,0);
+    set_PWM(3,0);
+
     return 0;
 }
